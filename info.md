8308cf43f80716d72829b1735327754c52fa4252
Ihor Chekhunov <ichekhunov.codica@gmail.com>
2022-07-14 10:53:46 +0200

    added swapper

--- info.md
+++ info.md
@@ -6,7 +6,27 @@ nest generate controller users
 
 nest generate service users
 
+***
 
 npm i @nestjs/config - чтоб настройки конфигурации вынести за пределы app.module
 
 cross-env - при запуске задавать системные переменные package.json
+
+***
+
+swagger - для документирования
+
+swagger-ui-express - для представления
+
+npm i @nestjs/swagger swagger-ui-express
+
+
+---
+
+---
+
+---
+
+---
+
+---
--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {Column, DataType, Model, Table} from 'sequelize-typescript'
 
 interface UserCreationAttrs {
@@ -6,19 +7,24 @@ interface UserCreationAttrs {
 }
 
 @Table({tableName: 'users'})
-export class User extends Model<User,  UserCreationAttrs> {
+export class User extends Model<User, UserCreationAttrs> {
+  @ApiProperty({example: '1', description: 'Unique identificator'})
   @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true })
   id: number;
-  
+
+  @ApiProperty({example: 'example@gmail.com', description: 'Email'})
   @Column({ type: DataType.STRING, unique: true, allowNull: false })
   email: string;
   
+  @ApiProperty({example: '12345678', description: 'Password'})
   @Column({ type: DataType.STRING, allowNull: false })
   password: string;
   
+  @ApiProperty({example: 'true', description: 'ban or not ban'})
   @Column({ type: DataType.BOOLEAN, defaultValue: false })
   banned: boolean;
 
+  @ApiProperty({example: 'For bad habits', description: 'Cause block'})
   @Column({ type: DataType.BOOLEAN, allowNull: true })  
   banReason: boolean;
 }
